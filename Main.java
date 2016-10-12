0bd5989fcfcfc6cffff487a5a715bf6f75f72446
Asagaida <myxa_a@mail.ru>
2016-10-12 23:18:57 +0300

    Добавил второго кота, для вывода на консоль

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Murzik", 2014, 8.15);
-        Cat cat2 = new Cat("Matilda", 2014, 5.84);
+        Cat cat1 = new Cat("Murzik", 2012, 8.15);
+        Cat cat2 = new Cat("Matilda", 2015, 5.84);
 
         System.out.println(cat1.toString());
-        cat2.toString();
+        System.out.println(cat2.toString());
         cat1.myaukanie();
     }
 }
